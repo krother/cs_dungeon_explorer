69a5d34e19cf274a1e2038ea01b28301d3075121
Kristian Rother <kristian@spiced-academy.com>
2024-01-12 17:27:32 +0100

    implement walls

---
+++ data_model.py
@@ -0,0 +1,105 @@
+"""
+Requirement: walls that stop the player.
+
+Code on github.com/krother/cs_dungeon_explorer
+"""
+import numpy as np
+import cv2
+import random
+from pydantic import BaseModel
+from typing import Any
+
+
+LEVEL_SIZE_X, LEVEL_SIZE_Y = 10, 10
+TILE_SIZE = 64  # measured in pixel
+
+SCREEN_SIZE_X, SCREEN_SIZE_Y = LEVEL_SIZE_X * TILE_SIZE, LEVEL_SIZE_Y * TILE_SIZE  # tuple
+X_BOUNDARY = SCREEN_SIZE_X - TILE_SIZE
+Y_BOUNDARY = SCREEN_SIZE_Y - TILE_SIZE
+
+
+# define the data model
+class Position(BaseModel):
+    x: int
+    y: int
+
+class Player(BaseModel):
+    image: Any  # TODO: look up how to use np.ndarray
+    position: Position
+
+
+class Level(BaseModel):
+    structure: list[list[bool]]  # <-- requires Py 3.10 or 3.9
+
+
+def double_size(img):
+    """returns an image twice as big"""
+    return np.kron(img, np.ones((2, 2, 1), dtype=img.dtype))
+
+
+wall = double_size(cv2.imread("tiles/wall.png"))
+
+def draw(player, level):
+    """draws the player image on the screen"""
+    frame = np.zeros((SCREEN_SIZE_X, SCREEN_SIZE_Y, 3), dtype=np.uint8)
+    # draw the player
+    xpos, ypos = player.position.x * TILE_SIZE, player.position.y * TILE_SIZE
+    if 0 <= xpos <= X_BOUNDARY and 0 <= ypos <= Y_BOUNDARY:
+        frame[ypos : ypos + TILE_SIZE, xpos : xpos + TILE_SIZE] = player.image
+
+    for y, row in enumerate(level.structure):
+        for x, cell in enumerate(row):
+            if cell:
+                xpos, ypos = x * TILE_SIZE, y * TILE_SIZE
+                if 0 <= xpos <= X_BOUNDARY and 0 <= ypos <= Y_BOUNDARY:
+                    frame[ypos : ypos + TILE_SIZE, xpos : xpos + TILE_SIZE] = wall
+
+
+
+    cv2.imshow("frame", frame)
+
+
+# TODO: do something about imread returning None values
+
+
+player = Player(
+    image = double_size(cv2.imread("tiles/deep_elf_high_priest.png")),
+    position = Position(x=4, y=4)
+)
+level = Level(
+    structure=[
+        [random.choice([True, False, False, False, False])
+         for x in range(10)]
+        for y in range(10)
+    ]
+)
+level.structure[4][4] = False
+
+
+exit_pressed = False
+
+while not exit_pressed:
+    draw(player, level)
+
+    # handle keyboard input
+    key = chr(cv2.waitKey(1) & 0xFF)
+    new_position = player.position
+    if key == "d":
+        new_position = Position(x=player.position.x + 1, y=player.position.y)
+    elif key == "a":
+        new_position = Position(x=player.position.x - 1, y=player.position.y)
+    elif key == "w" and player.position.y > 0:
+        new_position = Position(x=player.position.x, y=player.position.y -1)
+    elif key == "s" and player.position.y < LEVEL_SIZE_Y - 1:
+        new_position = Position(x=player.position.x, y=player.position.y + 1)
+    elif key == "q":
+        exit_pressed = True
+
+    # is there a wall?
+    if not level.structure[new_position.y][new_position.x]:
+        player.position = new_position
+
+cv2.destroyAllWindows()
+
+
+
