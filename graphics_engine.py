dc67e8a228c1fb64841efece8110d6009d676299
Kristian Rother <kristian@spiced-academy.com>
2024-01-26 13:50:06 +0100

    fix graphics

--- graphics_engine.py
+++ graphics_engine.py
@@ -3,6 +3,7 @@ Requirement: walls that stop the player.
 
 Code on github.com/krother/cs_dungeon_explorer
 """
+from game_logic import dungeon
 import numpy as np
 import cv2
 
@@ -20,64 +21,41 @@ def double_size(img):
     """returns an image twice as big"""
     return np.kron(img, np.ones((2, 2, 1), dtype=img.dtype))
 
-# TODO: handle file not found situation
-wall = double_size(cv2.imread("tiles/wall.png"))
+IMAGES = {
+    "player": double_size(cv2.imread("tiles/deep_elf_high_priest.png")),
+    "wall": double_size(cv2.imread("tiles/wall.png")),
+}
 
-def draw(player, level):
+def draw(dungeon):
     """draws the player image on the screen"""
     frame = np.zeros((SCREEN_SIZE_X, SCREEN_SIZE_Y, 3), dtype=np.uint8)
-    # draw the player
-    xpos, ypos = player.position.x * TILE_SIZE, player.position.y * TILE_SIZE
-    if 0 <= xpos <= X_BOUNDARY and 0 <= ypos <= Y_BOUNDARY:
-        frame[ypos : ypos + TILE_SIZE, xpos : xpos + TILE_SIZE] = player.image
-
-    for y, row in enumerate(level.structure):
-        for x, cell in enumerate(row):
-            if cell:
-                xpos, ypos = x * TILE_SIZE, y * TILE_SIZE
-                if 0 <= xpos <= X_BOUNDARY and 0 <= ypos <= Y_BOUNDARY:
-                    frame[ypos : ypos + TILE_SIZE, xpos : xpos + TILE_SIZE] = wall
+    for obj in dungeon.get_objects():
+        img = IMAGES[obj.name]
+        xpos, ypos = obj.position.x * TILE_SIZE, obj.position.y * TILE_SIZE
+        if 0 <= xpos <= X_BOUNDARY and 0 <= ypos <= Y_BOUNDARY:
+            frame[ypos : ypos + TILE_SIZE, xpos : xpos + TILE_SIZE] = img
 
     cv2.imshow("frame", frame)
 
 
 
-player = Player(
-    image = double_size(cv2.imread("tiles/deep_elf_high_priest.png")),
-    position = Position(x=4, y=4)
-)
-level = Level(
-    structure=[
-        [random.choice([True, False, False, False, False])
-         for x in range(10)]
-        for y in range(10)
-    ]
-)
-level.structure[4][4] = False
-
-
+MOVES = {
+    "d": "right",
+    "a": "left",
+    "w": "up",
+    "s": "down",
+}
 exit_pressed = False
-
 while not exit_pressed:
-    draw(player, level)
+    draw(dungeon)
 
     # handle keyboard input
     key = chr(cv2.waitKey(1) & 0xFF)
-    new_position = player.position
-    if key == "d":
-        new_position = Position(x=player.position.x + 1, y=player.position.y)
-    elif key == "a":
-        new_position = Position(x=player.position.x - 1, y=player.position.y)
-    elif key == "w" and player.position.y > 0:
-        new_position = Position(x=player.position.x, y=player.position.y -1)
-    elif key == "s" and player.position.y < LEVEL_SIZE_Y - 1:
-        new_position = Position(x=player.position.x, y=player.position.y + 1)
+    cmd = MOVES.get(key)
+    if cmd:
+        dungeon.execute_command(cmd)
     elif key == "q":
         exit_pressed = True
-
-    # is there a wall?
-    if not level.structure[new_position.y][new_position.x]:
-        player.position = new_position
 
 cv2.destroyAllWindows()
 
